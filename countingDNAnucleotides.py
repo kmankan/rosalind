4d97e41b03b95c3fc6816c3b5378ff3675360ea7
Malin Kankanamge <mttk@protonmail.com>
2023-12-07 14:58:35 +1100

    added an alternative approach that is more elegant

--- countingDNAnucleotides.py
+++ countingDNAnucleotides.py
@@ -3,11 +3,23 @@
 
 def countNucleotides(dna):
   dict = {"A":0, "C":0, "G":0, "T":0} #Harcode dictionary with A,C,G,T keys
-  nucleotides = list(dna)
-  for nt in nucleotides:
+  for nt in dna:
     if nt in dict:
       dict[nt] += 1 # increment value if it is found in list of nt's
 
   print(*list(dict.values()))
 
 countNucleotides(input())
+
+''' 
+An alternative approach that is much faster computationally
+
+Code:
+print(*map(input().count, "ACGT"))
+
+Explanation:
+- map(input().count, "ACGT"): This applies input().count to each of 'A', 'C', 'G', and 'T'. 
+It counts how many times each of these nucleotides appears in the DNA string entered by the user.
+- * (Unpacking Operator): The asterisk * is used to unpack the map object. 
+This turns the map object into individual elements.
+'''
